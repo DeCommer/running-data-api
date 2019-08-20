e13c1b1004b0b244fe15f99be1996d3500923487
Joseph DeCommer <joseph@Josephs-iMac.local>
2019-08-20 16:56:17 -0400

    Going over some errors

--- pgq.js
+++ pgq.js
@@ -29,8 +29,8 @@ const getRunById = (request, response) => {
 }
 
 const createRun = (request, response) => {
-    // const id = parseInt(request.params.id)
-    const {id, title, date, avg_pace, distance, duration, est_calories, location, notes } = request.body
+    const id = parseInt(request.params.id)
+    const {title, date, avg_pace, distance, duration, est_calories, location, notes } = request.body
 
     pool.query('INSERT INTO running_history (id, title, date, avg_pace, distance, duration, est_calories, location, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)', 
     [id, title, date, avg_pace, distance, duration, est_calories, location, notes], (error, results) => {
@@ -42,7 +42,8 @@ const createRun = (request, response) => {
 }
 
 const updateRun = (request, response) => {
-    const { id, title, date, avg_pace, distance, duration, est_calories, location, notes } = request.body
+    const id = parseInt(request.params.id)
+    const { title, date, avg_pace, distance, duration, est_calories, location, notes } = request.body
 
     pool.query(
         'UPDATE running_history SET id = $1, title = $2, date = $3, avg_pace = $4, distance = $5, duration = $6, est_calories = $7, location = $8, notes = $9 WHERE id = $10', 
